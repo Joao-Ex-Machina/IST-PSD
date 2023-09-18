769569cc0a37b96d9656bfb3a38d17d35e1636ba
Joao-Ex-Machina <joaobarreiroscoelhorodrigues@tecnico.ulisboa.pt>
2023-09-18 12:50:45 +0100

    Continued Lab 1. Changed FSM and added most mux logic to datapath

--- Lab1/control.vhd
+++ Lab1/control.vhd
@@ -10,12 +10,14 @@ entity control is
   port (
     clk, rst, exec : in std_logic;
     instr : in std_logic_vector (2 downto 0);
-    enables : out std_logic_vector (1 downto 0);
-    selectors : out std_logic_vector (1 downto 0));
+    op_code: out std_logic_vector(2 downto 0);
+    write_enable: out std_logic;
+    write_to: out std_logic;
+    reset: out std_logic;
 end control;
 
 architecture Behavioral of control is
-  type fsm_states is (s_initial, s_end, s_subs, s_adds, s_nand,s_nor,s_sra,s_load1,sload2 );
+  type fsm_states is (s_initial, s_end, s_subs, s_adds, s_nand,s_nor,s_sra,s_load1,s_load2,s_reset);
   signal currstate, nextstate: fsm_states;
 
 begin
@@ -23,7 +25,7 @@ begin
   begin 
     if clk'event and clk = '1' then
       if rst = '1' then
-        currstate <= s_initial ;
+        currstate <= s_reset;
       else
         currstate <= nextstate ;
       end if ;
@@ -37,57 +39,79 @@ begin
     
     case currstate is
       when s_initial =>
+          write_enable="0"
         if exec='1' then
-          if instr="111" then
+            write_enable="0"
+          if instr="000" then
             nextstate <= s_load2 ;
-          elsif instr="110" then
-	    nextstate <= s_nand;
-          elsif instr="101" then
-	    nextstate <= s_nor;
-	  elsif instr="100" then
-	    nextstate <= s_sra;
-          elsif instr="000" then
-            nextstate <= s_adds ;
           elsif instr="001" then
-            nextstate <= s_subs ;
-	  elsif instr="010" then
-            nextstate <= s_and;
-    	  elsif instr="011" then
-            nextstate <= s_load1 ;
+	    nextstate <= s_load1;
+          elsif instr="010" then
+	    nextstate <= s_adds;
+	  elsif instr="011" then
+	    nextstate <= s_subs;
+          elsif instr="100" then
+            nextstate <= s_muls ;
+          elsif instr="101" then
+            nextstate <= s_nand ;
+	  elsif instr="110" then
+            nextstate <= s_nor;
+    	  elsif instr="111" then
+            nextstate <= s_sra ;
 
           end if;
         end if;
-        selectors<="00";
-        enables<="00";
-        
-      when s_add =>
+
+        op_code<="000";
+        op_modifier<="0";
+        write_to<='1';
+
+    when s_load1 =>
         nextstate <= s_end;
-        selectors<="00";
-        enables<="10";
+        op_code<="000";
+        write_to<="0";
+
         
-      when s_sub =>
+    when s_load2 =>
         nextstate <= s_end;
-        selectors<="01";
-        enables<="10";
+        op_code<="000";
         
-      when s_and =>
+    when s_adds =>
         nextstate <= s_end;
-        selectors<="10";
-        enables<="10";
+        op_code<="001";
         
-      when s_load =>
+    when s_subs =>
         nextstate <= s_end;
-        selectors<="11";
-        enables<="01";
-        
-      when s_end =>
+        op_code<="010";
+       
+    when s_muls =>
+        nextstate <= s_end;
+        op_code<="011";
+    
+    when s_nand =>
+        nextstate <= s_end;
+        selectors<="100";
+        op_code<="01";
+       
+    when s_nor =>
+        nextstate <= s_end;
+        op_code<="101";
+       
+    when s_sra =>
+        nextstate <= s_end;
+        op_code<="110";
+
+    when s_end =>
         if exec='0' then
           nextstate <= s_initial ;
         end if;
-        selectors<="00";
-        enables<="00";
+
+        write_enable<="0"
+        op_code<="111"
+        write_to<="1";
 
     end case;
+
   end process;
 
 end Behavioral;
--- Lab1/datapath.vhd
+++ Lab1/datapath.vhd
@@ -4,18 +4,22 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity datapath is
     port (   
-      data_in : in std_logic_vector (7 downto 0);
-      sel_add_sub, sel_mux : in std_logic;
-      en_accum, en_r1 : in std_logic;
-      clk, rst_accum : in std_logic;
-      res, reg1 : out std_logic_vector (15 downto 0));
+      
+        data_in : in std_logic_vector (7 downto 0);
+        out_mux : in std_logic;
+        op_code : in std_logic(2 downto 0);
+        write_enable: in std_logic;
+        write_to : in std_logic;
+        clk, rst_accum : in std_logic;
+        res, reg1 : out std_logic_vector (15 downto 0);
+    );
 end datapath;
 
 architecture behavioral of datapath is
     signal res_addsub, res_and, res_alu : std_logic_vector (7 downto 0);
     signal res_addsub_sg, r1_sg, r2_sg : signed (7 downto 0);
     signal res_mul: signed (28 downto 0);
-    signal res_mul: signed (15 downto 0);
+    signal res_mul_cat: signed (15 downto 0);
     signal register2 : std_logic_vector (15 downto 0) := (others => '0'); -- this signal initialization is only considered for simulation
     signal register1 : std_logic_vector (15 downto 0) := (others => '0'); -- this signal initialization is only considered for simulation
 begin
@@ -24,31 +28,37 @@ begin
     r2_sg <= signed(register2);
 
     -- adder/subtracter
-        res_addsub <= std_logic_vector(res_addsub_sg);
-    res_addsub_sg <= r2_sg + r1_sg when sel_add_sub='0' else
-                     r2_sg - r1_sg;
+    res_add <= r2_sg + r1_sg
+    res_sub <= r2_sg - r1_sg;
     --multiplier
-    res_multiplier<=r1_sg*r2_sg
+    res_mul<=r1_sg*r2_sg
 
-    -- logic unit AND
-    res_and <= register1 and register2;
-    
     --logic unit NAND
     res_nand <= NOT(register1 and register2);
+    
     --logic unit NOR
     res_nor <= NOT (register and register2);
+    
+    -- barrel shifter
+    
+
     -- multiplexer
-    res_alu <= res_addsub when sel_mux='0' 
-               else res_and ;
+    res_mux <=  data_in when (op_code="000") else
+                res_add when (op_code="001") else
+                res_sub when (op_code="010") else
+                res_mul when (op_code="011") else
+                res_nand when (op_code="100") else
+                res_nor when (op_code="101") else
+                res_sra;
 
     -- register2
     process (clk)
     begin
         if clk'event and clk='1' then
-            if rst_reg='1' then
+            if reset='1' then
                 register2 <= (others => '0');
-            elsif en_r2 = '1' then
-                register2 <= data_in;
+            elsif  write_enable='1' AND write_to= '1' then
+                register2 <= res_mux;
             end if;
         end if;
     end process;
@@ -59,8 +69,8 @@ begin
         if clk'event and clk='1' then
             if rst_reg='1' then
                 register2 <= (others => '0');
-            elsif en_r1 = '1' then
-                register1 <= data_in;
+            elsif write_enable='1' AND write_to = '0' then
+                register1 <= res_mux;
             end if;
         end if;
     end process;
