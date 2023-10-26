504e8944701d19edd3054c4c173ed7c342a486b0
Francisco Simplício <reffranciscosimplicio@gmail.com>
2023-10-26 18:40:36 +0100

    control da layer 1 ta feito. adicionado contador e mem_we. vou apanhar ar já continuo

--- Lab3/Lab3.srcs/sources_1/new/control.vhd
+++ Lab3/Lab3.srcs/sources_1/new/control.vhd
@@ -43,16 +43,18 @@ entity control is
     address_enables : out std_logic_vector(3 downto 0); --[mem, w2, w1, p]
     address_resets : out std_logic_vector(3 downto 0); --[mem, w2, w1, p]
     
-    counter_enables : out std_logic_vector(3 downto 0); --[mem, w2, w1, p]
-    counter_resets : out std_logic_vector(3 downto 0); --[mem, w2, w1, p]
+    counter_enables : out std_logic_vector(4 downto 0); --[aux, mem, w2, w1, p]
+    counter_resets : out std_logic_vector(4 downto 0); --[aux, mem, w2, w1, p]
     
     muxpsel : out std_logic_vector(1 downto 0); --select which 8 bits are read
     muxw2sel : out std_logic_vector(1 downto 0); --same
     
+    mem_we : out std_logic; --activates the layer 1 memory for write
     reg_rst: out std_logic; --global reset
     write_enable: out std_logic_vector(1 downto 0); --[accum_layer2,accum_layer1]
     --FROM DATAPATH
     cp : in std_logic_vector(4 downto 0);
+    caux : in std_logic_vector(4 downto 0);
     cw1 : in std_logic_vector(1 downto 0);
     cw2 : in std_logic_vector(2 downto 0);
     cmem : in std_logic_vector(1 downto 0)
@@ -89,7 +91,67 @@ begin
                 next_state <= s_layer1;                 
             end if;
             --RESETS EM TUDO, ENABLES A ZERO
-        when s_layer1 =>
+            starter_address <= std_logic_vector(unsigned(img_number)*32);
+            address_enables <= (others => '0');
+            address_resets <= (others => '1');
+            counter_enables <= (others => '0');
+            counter_resets <= (others => '1');
+            muxpsel <= (others => '0');
+            muxw2sel <= (others => '0');
+            reg_rst <= '1';
+            write_enable <= (others => '0');
+            mem_we <= '0';
+    when s_layer1 =>
+            if unsigned(caux) <= 30 then
+                next_state <= s_layer1;
+                if unsigned(cp) <= 30 then
+                    if unsigned(cw1) <= 2 then
+                        starter_address <= std_logic_vector(unsigned(img_number)*32);
+                        address_enables <= "0010";
+                        address_resets <= (others => '0');
+                        counter_enables <= "00010";
+                        counter_resets <= (others => '0');
+                        muxpsel <= cw1;
+                        muxw2sel <= (others => '0');
+                        reg_rst <= '0';
+                        write_enable <= "01"; --JOAO ISTO TA CERTO?
+                        mem_we <= '0';
+                    else --cw1==3
+                        starter_address <= std_logic_vector(unsigned(img_number)*32);
+                        address_enables <= "0001";
+                        address_resets <= "0000";
+                        counter_enables <= "00001";
+                        counter_resets <= "00010";
+                        muxpsel <= cw1;
+                        muxw2sel <= (others => '0');
+                        reg_rst <= '0';
+                        write_enable <= "01"; --JOAO ISTO TA CERTO?
+                        mem_we <= '0';
+                    end if;
+                else --cp==31
+                    starter_address <= std_logic_vector(unsigned(img_number)*32);
+                    address_enables <= "1000";
+                    address_resets <= "0010";
+                    counter_enables <= "10000";
+                    counter_resets <= "00011";
+                    muxpsel <= cw1;
+                    muxw2sel <= (others => '0');
+                    reg_rst <= '0';
+                    write_enable <= "01"; --JOAO ISTO TA CERTO?
+                    mem_we <= '1';
+                end if;
+            else --caux==31
+                next_state <= s_layer2;starter_address <= std_logic_vector(unsigned(img_number)*32);
+                address_enables <= "0000";
+                address_resets <= "1111";
+                counter_enables <= "00000";
+                counter_resets <= "11111";
+                muxpsel <= cw1;
+                muxw2sel <= (others => '0');
+                reg_rst <= '0';
+                write_enable <= "01"; --JOAO ISTO TA CERTO?
+                mem_we <= '1';
+            end if;
             
         when s_layer2 =>
         
