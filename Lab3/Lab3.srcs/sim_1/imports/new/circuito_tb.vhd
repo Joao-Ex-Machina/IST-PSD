2c40d80893d000db6a4dbe179aa9f2add7bcfe7a
Francisco Simplício <reffranciscosimplicio@gmail.com>
2023-10-27 15:41:28 +0100

    tb fixed acho

--- Lab3/Lab3.srcs/sim_1/imports/new/circuito_tb.vhd
+++ Lab3/Lab3.srcs/sim_1/imports/new/circuito_tb.vhd
@@ -51,9 +51,9 @@ signal rst : std_logic := '0';
 signal init : std_logic := '0';
 signal img_number : std_logic_vector(3 downto 0) := (others => '0');
 -- Outputs
-signal done : std_logic;
+--signal done : std_logic;
 signal data_out : std_logic_vector(3 downto 0);
-signal cp, cw1, cw2, cmem, caux1, caux2 : std_logic_vector(5 downto 0);
+--signal cp, cw1, cw2, cmem, caux1, caux2 : std_logic_vector(5 downto 0);
 -- Clock period definitions
 constant clk_period : time := 10 ns;
     
@@ -62,9 +62,10 @@ begin
     uut : circuito port map(
         clk => clk, rst => rst,
         init => init, img_number => img_number, 
-        done => done, data_out => data_out,
-        cp => cp, cw1 => cw1, cw2 => cw2,
-        cmem => cmem, caux1 => caux1, caux2 => caux2
+        --done => done, 
+        data_out => data_out
+        --cp => cp, cw1 => cw1, cw2 => cw2,
+        --cmem => cmem, caux1 => caux1, caux2 => caux2
     );
     
     clk <= not clk after clk_period/2;
@@ -75,7 +76,7 @@ begin
         wait for clk_period * 10;
         rst <= '1' after 20 ns,
                 '0' after 40 ns;
-        img_number <=  "0000" after 40 ns;
+        img_number <=  "0000000" after 40 ns;
         
         init <= '1' after 40 ns,
                 '0' after 80 ns;
